cbfe6eddfbcc80b15cd6d7b7e3f77d3dcdcc9888
carlosgarcia32 <69259477+carlosgarcia32@users.noreply.github.com>
2022-12-06 00:18:19 -0500

    Updated Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { Nav, Navbar, NavDropdown, Container, Button, Offcanvas } from 'react-bootstrap'
+import { Nav, Navbar, NavDropdown, Container, Button, Offcanvas, Stack } from 'react-bootstrap'
 
 import {MdDeveloperMode} from 'react-icons/md'
 import {BiRefresh, BiLogOut} from 'react-icons/bi'
+import { CgMenuGridO } from 'react-icons/cg'
+
 import { Link } from 'react-router-dom'
 
 function NavBar({ expandedMenu, handleCollapse, handleExpand, handleRefresh, handleLogout, setShowAddBudgetModal, setShowAddExpenseModal, setShowAddIncomeModal}) {
@@ -10,18 +12,21 @@ function NavBar({ expandedMenu, handleCollapse, handleExpand, handleRefresh, han
     <Navbar bg="light" variant="light" expand="sm" className="nav-bar" expanded={expandedMenu}>
         <Container>
           <Navbar.Brand className='title nav-bar' href=''>
-              BudgetingWorks
-              <p className='me-auto subtitle'><MdDeveloperMode className='subtitle'/> Site is under development</p>
+            BudgetingWorks
+            <p className='me-auto subtitle'><MdDeveloperMode className='subtitle'/> Site is under development</p>
           </Navbar.Brand>
+          <Stack className='' direction='horizontal'>
           <Nav.Item>
             <Button variant='outline-secodary'><BiRefresh className='fs-1'
                     onClick={handleRefresh}/></Button>
           </Nav.Item>
-          <Navbar.Toggle 
-            aria-controls='basic-navbar-nav' 
-            className='nav-bar' 
-            onClick={() => handleExpand()}/>
-          <Navbar.Offcanvas className=''>
+          <Button variant='outline-secodary' onClick={() => {
+                handleExpand()
+              }} className=''>
+              <CgMenuGridO className='fs-1' />
+          </Button>
+          </Stack>
+          <Offcanvas className='' show={expandedMenu} onHide={handleCollapse}>
             <Container>
               <Offcanvas.Header closeButton onClick={() => handleCollapse()}>
                 <Offcanvas.Title id={'offcanvasNavbarLabel-expand-sm'}>
@@ -76,7 +81,7 @@ function NavBar({ expandedMenu, handleCollapse, handleExpand, handleRefresh, han
                 </Nav>
               </Offcanvas.Body>
             </Container>
-          </Navbar.Offcanvas>
+          </Offcanvas>
         </Container>
     </Navbar>
   )
